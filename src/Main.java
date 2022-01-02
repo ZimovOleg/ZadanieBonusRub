15e19b332c247244425bfaff5e084dc1d5a6724d
ZimovOleg <zimov.oleg@gmail.com>
2022-01-02 10:40:51 +0400

    src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,20 @@ public class Main {
     public static void main(String[] args) {
 
         int check = 100; // Начальный баланс
-        int payment = 1100; // Сумма пополнения
+        int payment = 1000; // Сумма пополнения
         int bonus; // кол-во бонусных рублей
         int balanc; // Итоговый баланс
 
         if (payment > 999) { bonus = (payment / 100);
-            balanc = (bonus + payment + check);
-            System.out.println("Поздравляем Вы получили:");
-            System.out.println(bonus + " бонусных рублей");
-            System.out.println("Ваш баланс:");
-            System.out.println(balanc + " бонусных рублей");}
-        else {
-            balanc = (payment + check);
-            System.out.println("Бонусные рубли не начисленны, Ваш баланс:");
-            System.out.println(balanc + " рублей");
-
+            }
+        else { bonus = ( 0 );
         }
+
+        balanc = (bonus + payment + check);
+        System.out.println("Поздравляем Вы получили:");
+        System.out.println(bonus + " бонусных рублей");
+        System.out.println("Ваш баланс:");
+        System.out.println(balanc + "  рублей");
 
     }
 }
